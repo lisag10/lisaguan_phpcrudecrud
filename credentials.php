a5e6ed7b352f4ba3986a1272a9ee3c80a4563416
Lisa Guan <guan0178@umn.edu>
2025-10-22 02:41:58 +0000

    initial commit

---
+++ credentials.php
@@ -0,0 +1,7 @@
+<?php
+//remember to populate the data here
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
+$dbname = '';              //which db you're going to use
+$username = '';
+$password = '';
+?>
